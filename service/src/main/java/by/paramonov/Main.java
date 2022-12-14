c96f0728a268bd4caa64f71c7c0e1ea1b29715be
Alexey Paramonov <paramonov_tama@mail.ru>
2022-12-14 20:21:19 +0300

    feat: compose a check form

--- service/src/main/java/by/paramonov/Main.java
+++ service/src/main/java/by/paramonov/Main.java
@@ -5,18 +5,19 @@ import by.paramonov.entity.Check;
 import java.util.*;
 
 public class Main {
-
+    public static Map<Integer, List<String>> priceList = new HashMap<>();
     public static void main(String[] args) {
-        Map<Integer, List<String>> priceList = new HashMap<>();
-        priceList.put(1, new LinkedList<>(Arrays.asList("24.2", "milk")));
-        priceList.put(2, new LinkedList<>(Arrays.asList("35.2", "cheese")));
-        priceList.put(3, new LinkedList<>(Arrays.asList("2.3", "bread")));
-        priceList.put(4, new LinkedList<>(Arrays.asList("154.99", "spear")));
-        priceList.put(5, new LinkedList<>(Arrays.asList("1.0", "button")));
+
+        priceList.put(1, new LinkedList<>(Arrays.asList("24.2", "Milk")));
+        priceList.put(2, new LinkedList<>(Arrays.asList("35.2", "Cheese")));
+        priceList.put(3, new LinkedList<>(Arrays.asList("2.3", "Bread")));
+        priceList.put(4, new LinkedList<>(Arrays.asList("154.99", "Spear")));
+        priceList.put(5, new LinkedList<>(Arrays.asList("1.0", "Button")));
 
         System.out.println(priceList.get(3));
 
         Check check = new Check();
-        check.printCheck(args);
+        String[] argsForDebug = new String[] {"3-5", "4-25"};
+        check.printCheck(argsForDebug);
     }
 }
--- service/src/main/java/by/paramonov/entity/Check.java
+++ service/src/main/java/by/paramonov/entity/Check.java
@@ -1,22 +1,40 @@
 package by.paramonov.entity;
 
 
+import by.paramonov.Main;
+
+import java.util.List;
+
 public class Check {
+    static double vat = 20;
+    static double totalPrice = 0;
+    static double discount = 0.01;
 
     public void printCheck(String[] inputArgs) {
+
         if (inputArgs.length != 0) {
             System.out.println("CASH RECEIPT");
-            System.out.println("================");
+            System.out.println("=========================================");
             System.out.println("QTY"
                     + "\tDESCRIPTION"
-                    + "\tPRICE"
-                    + "\tTOTAL");
+                    + "\t\t\tPRICE"
+                    + "\t\tTOTAL");
             for (int i = 0; i < inputArgs.length; i++) {
                 if (Character.isDigit(inputArgs[i].charAt(0))) {
                     String[] split = inputArgs[i].split("-");
-                    System.out.println(split[0]);
+                    List<String> strings = Main.priceList.get(Integer.valueOf(split[0]));
+                    int quantity = Integer.parseInt(split[1]);
+                    double price = Double.parseDouble(strings.get(0));
+                    double total = quantity * price;
+                    if (quantity > 5) {
+                        total *= discount;
+                    }
+                    totalPrice += total;
+                    String description = strings.get(1);
+                    System.out.println(quantity + "\t" + description + "\t\t\t\t$" + price + "\t\t$" + total);
                 }
             }
+            System.out.println("=========================================");
         }
     }
 }
